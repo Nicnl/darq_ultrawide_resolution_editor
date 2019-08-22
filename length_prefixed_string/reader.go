fde2769a4891b8aacb9890a3ad1d7a19346fd13c
Lim Nicolas <nicnl25@gmail.com>
2019-08-22 15:55:55 +0200

    Simple LengthPrefixedString implemented

--- length_prefixed_string/reader.go
+++ length_prefixed_string/reader.go
@@ -2,16 +2,37 @@ package length_prefixed_string
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 )
 
-func Read(r io.Reader) (string, error) {
+func readLength(r io.Reader) (uint32, error) {
 	var b uint8
 
 	err := binary.Read(r, binary.LittleEndian, &b)
 	if err != nil {
+		return 0, err
+	}
+
+	// 'The high bit MUST be used to indicate that the length continues in the next byte.'
+	if b&1 > 128 {
+		return 0, fmt.Errorf("not implemented, todo")
+	}
+
+	return uint32(b), nil
+}
+
+func Read(r io.Reader) (string, error) {
+	l, err := readLength(r)
+	if err != nil {
 		return "", err
 	}
 
-	return "", nil
+	b := make([]byte, l)
+	_, err = r.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
